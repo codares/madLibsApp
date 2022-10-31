d7c596f037bb0a1709a8a6fc50188c565f6913b9
codares <driety013@gmail.com>
2022-10-31 14:38:47 +0800

    (Its getting worse adding for loop)

--- app.js
+++ app.js
@@ -8,5 +8,22 @@ Word got out that the ${userInput[3]} had "debugged" the Entire ${userInput[4]}`
 
 console.log(originalStory);
 
+
+
 let numberOfQuestions = 5;
+let question = prompt("Enter a ");
+
+let questionArray =[`"${question} Place or Country..",
+"${question} a name..",
+"${question} place of work or Company..",
+"${question} a team name..",
+"${question} code problem name"
+`]
+
+// Question Counter (which question are we on)
+let questionCounter = 0;
+
+for(let i = numberOfQuestions;i>0;i--){
+    console.log(i);
+}
 
