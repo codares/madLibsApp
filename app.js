302d7bcdac914872be83ddcca259b376d5b52408
codares <driety013@gmail.com>
2022-10-31 13:31:56 +0800

    (Added mad-lib replacement variables to story)

---
+++ app.js
@@ -0,0 +1,9 @@
+//The orignal
+//let originalStory = 'In Philippines, computer pioneer Leirey found herself working on a Company. Word got out that the team had "debugged" the Entire Mistake';
+
+//Empty array to add user input to it
+let userInput =[];
+let originalStory = `In ${userInput[0]}, computer pioneer ${userInput[01]} found herself working on a ${userInput[2]}.
+Word got out that the ${userInput[3]} had "debugged" the Entire ${userInput[0]}`;
+
+console.log(originalStory);
