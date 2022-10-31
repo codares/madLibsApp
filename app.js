e6f40c045dad028aaf6c25a87c9a92e1b5f3bb9c
codares <driety013@gmail.com>
2022-10-31 13:41:22 +0800

    (Adding the questions array)

--- app.js
+++ app.js
@@ -4,6 +4,16 @@
 //Empty array to add user input to it
 let userInput =[];
 let originalStory = `In ${userInput[0]}, computer pioneer ${userInput[01]} found herself working on a ${userInput[2]}.
-Word got out that the ${userInput[3]} had "debugged" the Entire ${userInput[0]}`;
+Word got out that the ${userInput[3]} had "debugged" the Entire ${userInput[4]}`;
 
 console.log(originalStory);
+
+let numberOfQuestions =[];
+numberOfQuestions[0];
+numberOfQuestions[1];
+numberOfQuestions[2];
+numberOfQuestions[3];
+numberOfQuestions[4];
+
+
+console.log(numberOfQuestions);
