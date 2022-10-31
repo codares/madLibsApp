67ce8c8ca4edaca24bc920ed6f4e18572441e235
codares <driety013@gmail.com>
2022-10-31 15:48:55 +0800

    (All bug is fix here..)

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+"use strict";
+
 //The orignal
 //let originalStory = 'In Philippines, computer pioneer Leirey found herself working on a Company. Word got out that the team had "debugged" the Entire Mistake';
 
 //Empty array to add user input to it
 let userInput =[];
-let originalStory = `In ${userInput[0]}, computer pioneer ${userInput[01]} found herself working on a ${userInput[2]}.
+let originalStory = `In ${userInput[0]}, computer pioneer ${userInput[1]} found herself working on a ${userInput[2]}.
 Word got out that the ${userInput[3]} had "debugged" the Entire ${userInput[4]}`;
 
 console.log(originalStory);
@@ -13,22 +15,28 @@ console.log(originalStory);
 let numberOfQuestions = 5;
 let question = "Enter a ";
 
-let questionArray =[`"${question} Place or Country..",
-"${question} a name..",
-"${question} place of work or Company..",
-"${question} a team name..",
-"${question} code problem name"
-`]
+let questionArray =[`${question} Place or Country..`,
+`${question} a name..`,
+`${question} place of work or Company..`,
+`${question} a team name..`,
+`${question} code problem name..`
+];
 
 // Question Counter (which question are we on)
 let questionCounter = 0;
 
-for(let i = numberOfQuestions;i>=0;i--){
+for(let i = numberOfQuestions; i > 0; i--){
     //console.log(i);
 
     //display in the console log if running correctly
+    /*
     console.log(
         questionArray[questionCounter] + `....(${numberOfQuestions} questions leaf)`
+        );
+    numberOfQuestions--;
+    */
+    prompt(
+        questionArray[questionCounter] + `....(${numberOfQuestions} questions left)`
         );
     numberOfQuestions--;
     questionCounter++;
